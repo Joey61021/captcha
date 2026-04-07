4ce033a3428e1a3e30696576d39503124c546f0b
Joey61021 <joseph.gardiner58@gmail.com>
2026-04-07 18:00:07 +1000

    Update permissions value

--- src/main/java/com/captcha/plugin/globals/Permissions.java
+++ src/main/java/com/captcha/plugin/globals/Permissions.java
@@ -1,5 +1,5 @@
 package com.captcha.plugin.globals;
 
 public final class Permissions {
-	public static final String COMMAND_CAPTCHA = "em.command.captcha";
+	public static final String COMMAND_CAPTCHA = "captcha.command.captcha";
 }
